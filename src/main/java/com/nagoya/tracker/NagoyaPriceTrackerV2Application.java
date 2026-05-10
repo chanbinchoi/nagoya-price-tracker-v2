48fcdf30f9523e4dafb1d12395d8b2d7b7112085
chanbinchoi <classy9611@gmail.com>
2026-05-11 07:30:39 +0900

    First commit

---
+++ src/main/java/com/nagoya/tracker/NagoyaPriceTrackerV2Application.java
@@ -0,0 +1,13 @@
+package com.nagoya.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagoyaPriceTrackerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NagoyaPriceTrackerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nagoya/tracker/NagoyaPriceTrackerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagoya.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagoyaPriceTrackerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
